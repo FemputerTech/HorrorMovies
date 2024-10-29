aeeeded7a585a72a7c7025eb7b1e57da6c0532a6
FemputerTech <mleicht92@gmail.com>
2024-10-29 13:59:49 -0700

    typo

--- frontend/src/components/Sidebar/subcomponents/Logo.js
+++ frontend/src/components/Sidebar/subcomponents/Logo.js
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="logo">
-      <h2 class="logo-title">Creepy Cinema</h2>
+      <h2 className="logo-title">Creepy Cinema</h2>
     </div>
   );
 };
