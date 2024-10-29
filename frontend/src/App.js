92be14ecb77096454efff9279cf7a57daaa9bc4a
FemputerTech <mleicht92@gmail.com>
2024-10-29 11:57:10 -0700

    removed the s

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import Header from "./components/Headers/Header";
-import Sidebar from "./components/Sidebars/Sidebar";
-import Main from "./components/Mains/Main";
-import Footer from "./components/Footers/Footer";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
