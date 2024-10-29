578236136881090570525f1dee204c7d55878786
FemputerTech <mleicht92@gmail.com>
2024-10-29 12:26:15 -0700

    basic setup for the sidebar

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,11 +1,43 @@
 import React from "react";
+import MovieLists from "./subcomponents/MovieLists";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+  const decades = [
+    "1920s",
+    "1930s",
+    "1940s",
+    "1950s",
+    "1960s",
+    "1970s",
+    "1980s",
+    "1990s",
+    "2000s",
+    "2010s",
+    "2020s",
+  ];
+  const subgenres = [
+    "Analog Horror",
+    "Body Horror",
+    "Comedy Horror",
+    "Creature Feature",
+    "Folk Horror",
+    "Found Footage",
+    "Giallo",
+    "Gothic",
+    "Occult",
+    "Paranormal",
+    "Pyschological Horror",
+    "Slasher",
+    "Supernatural Horror",
+    "Survival Horror",
+    "Vampire",
+    "Zombie Feature",
+  ];
   return (
     <div className="sidebar">
       <div className="logo-container">Creepy Cinema</div>
-      <div className="movie-lists">Movie Filters</div>
+      <MovieLists decades={decades} subgenres={subgenres} />
     </div>
   );
 };
---
+++ frontend/src/components/Sidebar/subcomponents/MovieLists.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const MovieLists = ({ decades, subgenres }) => {
+  return (
+    <div className="movie-lists">
+      <ul className="filters">
+        <li className="filter-item">
+          <h3 className="filter-title">Decades</h3>
+          <ul className="list">
+            {decades.map((decade, index) => (
+              <li className="list-item" key={index}>
+                {decade}
+              </li>
+            ))}
+          </ul>
+        </li>
+        <li className="filter-item">
+          <h3 className="filter-title">Subgenres</h3>
+          <ul className="list">
+            {subgenres.map((subgenre, index) => (
+              <li className="list-item" key={index}>
+                {subgenre}
+              </li>
+            ))}
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default MovieLists;
