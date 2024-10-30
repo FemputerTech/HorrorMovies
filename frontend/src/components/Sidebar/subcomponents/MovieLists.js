c41e3e95bf7cc46d2275f1a06eccd85eee09953d
FemputerTech <mleicht92@gmail.com>
2024-10-30 13:01:45 -0700

    worry about subgenres for now

--- frontend/src/components/Sidebar/subcomponents/MovieLists.js
+++ frontend/src/components/Sidebar/subcomponents/MovieLists.js
@@ -5,37 +5,17 @@ const MovieLists = ({ decades, subgenres, onListClick }) => {
     <div className="movie-lists">
       <ul className="filters">
         <li className="filter-item">
-          <h3>Popular</h3>
-          <ul className="list">
-            <li className="list-item" onClick={() => onListClick("Top 100")}>
-              Top 100
-            </li>
-          </ul>
-        </li>
-        <li className="filter-item">
           <h3 className="filter-title">Subgenres</h3>
           <ul className="list">
             {subgenres.map((subgenre, index) => (
               <li
                 className="list-item"
-                key={index}
-                onClick={() => onListClick(subgenre)}
+                key={subgenre.key}
+                onClick={() =>
+                  onListClick(subgenre.key, subgenre.label, "subgenre")
+                }
               >
-                {subgenre}
-              </li>
-            ))}
-          </ul>
-        </li>
-        <li className="filter-item">
-          <h3 className="filter-title">Decades</h3>
-          <ul className="list">
-            {decades.map((decade, index) => (
-              <li
-                className="list-item"
-                key={index}
-                onClick={() => onListClick(decade)}
-              >
-                {decade}
+                {subgenre.label}
               </li>
             ))}
           </ul>
