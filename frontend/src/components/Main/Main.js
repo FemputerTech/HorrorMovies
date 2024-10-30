5fc452cd43b36e0dc3cd627ec71e0c11ae247af7
FemputerTech <mleicht92@gmail.com>
2024-10-30 13:02:30 -0700

    setup fetchMovies and turned off pointer events for the images to get rid of cors issue

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -5,44 +5,48 @@ const Main = ({ selectedList }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    // Fetch data from the endpoint
+    // Function to fetch movies from the seleted endpoint
     const fetchMovies = async () => {
-      try {
-        const response = await fetch("http://localhost:5000/movies/top100");
-        if (!response.ok) {
-          throw new Error("Failed to fetch movies");
+      if (selectedList.key) {
+        try {
+          const response = await fetch(
+            `http://localhost:5000/movies/${selectedList.endpoint}/${selectedList.key}`
+          );
+          if (!response.ok) {
+            throw new Error("Failed to fetch movies");
+          }
+          const data = await response.json();
+          setMovies(data);
+        } catch (error) {
+          console.error("Error fetching movies:", error);
         }
-        const data = await response.json();
-        setMovies(data);
-      } catch (error) {
-        console.error("Failed to fetch movies from server:", error);
       }
     };
 
-    if (selectedList) {
-      if (selectedList === "Top 100") {
-        fetchMovies();
-      }
-    }
+    fetchMovies();
   }, [selectedList]);
 
   return (
     <div className="main">
-      <h2>{selectedList}</h2>
-      {selectedList === "Top 100" && (
-        <ul className="movie-list">
-          {movies.map((movie) => (
-            <li key={movie._id} className="movie-item">
-              <img
-                src={`${movie.poster}`}
-                alt={movie.title}
-                className="movie-poster"
-              />
-              <h3>{movie.title}</h3>
-            </li>
-          ))}
-        </ul>
-      )}
+      <h2 className="movie-list-heading">{selectedList.name}</h2>
+      <ul className="movies">
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <h3 className="movie-title">{movie.title}</h3>
+            <img
+              className="movie-image"
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w92${movie.poster_path}`
+                  : ""
+              }
+              alt={movie.title}
+              referrerPolicy="strict-origin-when-cross-origin"
+              // crossOrigin="use-credentials"
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
