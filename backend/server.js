93306859bb1958ceb48d0a865c132b1bea993271
FemputerTech <mleicht92@gmail.com>
2024-10-30 12:59:17 -0700

    setup cors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ dotenv.config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const schedule = require("node-schedule");
 const cors = require("cors");
 const movieRoutes = require("./routes/movieRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -16,28 +15,21 @@ const app = express();
 const PORT = config.port || process.env.PORT || 5000;
 
 // Middleware
-app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"], // Add other domains as needed
+  })
+);
 
 // Connect to MongoDb
 const dbURI = process.env.MONGODB_URI;
 mongoose
   .connect(dbURI)
-  .then((result) => console.log("Connected to MongoDB"))
+  .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.log("MongoDB connection error:", error));
 
-// Schedule job to update top 100 horror movies on the last day of each month at 11:59 PM
-const job = schedule.scheduleJob("0 59 23 L * *", async () => {
-  console.log("Starting the update of top 100 horror movies...");
-  try {
-    await storeTop100();
-    console.log("Top 100 horror movies updated successfully.");
-  } catch (error) {
-    console.error("Error updating top 100 movies:", error);
-  }
-});
-
 app.get("/", (req, res) => {
-  console.log("Welcome to my horror movie recommendation site");
+  res.send("Welcome to my horror movie recommendation site");
 });
 
 app.use("/movies", movieRoutes);
