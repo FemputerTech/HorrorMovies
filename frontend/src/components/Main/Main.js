b10ee61400ad5bc2e2054339f1c9ada498cc8901
FemputerTech <mleicht92@gmail.com>
2024-10-30 13:21:22 -0700

    started the implementation for the movie card

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import MovieCard from "./subcomponents/MovieCard";
 import "./Main.css";
 
 const Main = ({ selectedList }) => {
@@ -32,18 +33,7 @@ const Main = ({ selectedList }) => {
       <ul className="movies">
         {movies.map((movie) => (
           <li key={movie.id}>
-            <h3 className="movie-title">{movie.title}</h3>
-            <img
-              className="movie-image"
-              src={
-                movie.poster_path
-                  ? `https://image.tmdb.org/t/p/w92${movie.poster_path}`
-                  : ""
-              }
-              alt={movie.title}
-              referrerPolicy="strict-origin-when-cross-origin"
-              // crossOrigin="use-credentials"
-            />
+            <MovieCard movie={movie} />
           </li>
         ))}
       </ul>
