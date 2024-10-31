00c7331d6e061730127061a9bd1bfe556d4adc1d
FemputerTech <mleicht92@gmail.com>
2024-10-31 11:51:58 -0700

    removed console log

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -7,9 +7,6 @@ const Main = ({ selectedList }) => {
 
   useEffect(() => {
     const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-    console.log(
-      `Fetching from: ${BACKEND_URL}/movies/${selectedList.endpoint}/${selectedList.key}`
-    );
 
     // Function to fetch movies from the seleted endpoint
     const fetchMovies = async () => {
