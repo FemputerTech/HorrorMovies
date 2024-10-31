c03913b18b5762a384dc7db14f40a17d9d5a0298
FemputerTech <mleicht92@gmail.com>
2024-10-31 11:09:59 -0700

    updated project url

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://horrormovies-frontend-754209826365.us-west1.run.app",
+      "https://horrormovies-754209826365.us-west1.run.app",
     ],
   })
 );
