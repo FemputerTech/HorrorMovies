d05490a2b562afc7396124e9d00e4065c4795d7a
FemputerTech <mleicht92@gmail.com>
2024-11-02 17:35:27 -0700

    started implementing the user service

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -6,4 +6,32 @@
  * and more maintainable code.
  */
 const express = require("express");
+const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+
+class UserService {
+  /**
+   * Create a new user
+   * @param {Object} userData - User data including email and password
+   * @returns
+   * @throws {Error} Throws an error if the user creation fails.
+   */
+  async createUser(userData) {
+    const { email, password } = userData;
+    console.log(email, password);
+  }
+
+  async updateUser(userId, userData) {
+    console.log("updating user...");
+  }
+
+  async deleteUser(userId) {
+    console.log("deleting user...");
+  }
+
+  async findUser(userId) {
+    console.log("finding user...");
+  }
+}
+
+module.exports = new UserService();
