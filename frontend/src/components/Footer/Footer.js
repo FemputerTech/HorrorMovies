e87de59d452c0348b62c171c5f88729bc7905067
FemputerTech <mleicht92@gmail.com>
2024-11-04 16:03:46 -0800

    added the footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -2,7 +2,9 @@ import React from "react";
 import "../../styles/components/Footer.css";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">&copy; 2024 FemputerTech. All rights reserved.</div>
+  );
 };
 
 export default Footer;
