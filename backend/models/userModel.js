60272cd888ccdaafc5594df833321bd9c8053e71
FemputerTech <mleicht92@gmail.com>
2024-11-06 12:19:58 -0800

    uncommented out the userId for the userMovies schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 
 const userMovieSchema = new mongoose.Schema({
   tmdbId: { type: Number, required: true },
-  // userId: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "User",
-  //   required: true,
-  // },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   title: { type: String, required: true },
   status: {
     type: String,
